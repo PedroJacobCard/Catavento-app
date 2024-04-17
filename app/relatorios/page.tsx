0d3a47a498d1891409729b8afd5b7fc00fb9f174
Pedro Henrique Jacob Cardoso <pedrojacob05cardoso@gmail.com>
2024-04-17 18:57:07 -0300

    updated report

--- app/relatorios/page.tsx
+++ app/relatorios/page.tsx
@@ -11,6 +11,7 @@ import Link from "next/link";
 import Navbar from "@/app/components/Navbar";
 import RememberField from "@/app/components/RememberField";
 import Footer from "../components/Footer";
+import DownloadReportTable from "../components/downloadFiles/DownloadReportTable";
 
 //import lib functions
 import ShowShadow from "@/lib/ShowShadow";
@@ -18,12 +19,6 @@ import ShowShadow from "@/lib/ShowShadow";
 //import costume hooks
 import useReport from "../hooks/useReport";
 
-//import types
-import DownloadReport from "../components/downloadFiles/DownloadReport";
-
-//importar bibliotecas para download de relatórios
-import html2canvas from "html2canvas";
-import jsPDF from "jspdf";
 
 function Report() {
   //Funcionalidades para display do campo de lembretes
@@ -31,49 +26,6 @@ function Report() {
 
   //importar relatórios
   const { reports } = useReport();
-
-  //funcionalidades para baixar os relatórios
-  const downloadLinks = useRef<RefObject<HTMLDivElement>[]>([]);
-  const [, setForceUpdate] = useState(Date.now());
-
-  const handleDownload = async (reportIndex: number) => {
-    const inputData = downloadLinks.current[reportIndex].current;
-    
-    try {
-      if (!inputData) throw new Error("Não foi possível acessar o link");
-
-      const canvas = await html2canvas(inputData);
-      const imgData = canvas.toDataURL("image/png");
-
-      const pdf = new jsPDF({
-        orientation: "portrait",
-        unit: "px",
-        format: "a4",
-      });
-
-      const pdfWidth = pdf.internal.pageSize.getWidth();
-      const pdfHeight = pdf.internal.pageSize.getHeight();
-
-      const width = pdfWidth - 150;
-      const height = (canvas.height * width) / canvas.width;
-
-      const xPosition = (pdfWidth - width) / 2;
-      const yPosition = (pdfHeight - height) / 2;
-
-      pdf.addImage(imgData, "PNG", xPosition, yPosition, width, height);
-      pdf.save("Relatório.pdf");
-    } catch (error) {
-      console.error('Falha ao fazer o download', error)
-    }
-  }
-
-  useEffect(() => {
-    // Ensure downloadLinks.current is an array before //assigning new refs
-    if (Array.isArray(downloadLinks.current)) {
-      downloadLinks.current = reports!.map(() => createRef<HTMLDivElement>());
-    }
-    setForceUpdate(Date.now())
-  }, [reports]);
 
   return (
     <>
@@ -110,17 +62,74 @@ function Report() {
         {reports &&
           reports.map((report, reportIndex) => (
             <div key={reportIndex}>
-              <div ref={downloadLinks.current[reportIndex]}>
-                <DownloadReport report={report} reportIndex={reportIndex} />
-              </div>
-
-              <button
-                type="button"
-                className="mx-2 md:mx-8 p-2 flex items-center w-[20vw] gap-3 m-auto py-1 px-2 mt-2 shadow-md dark:bg-darkMode bg-primaryBlue rounded-md dark:hover:bg-darkModeBgColor hover:bg-secondaryBlue duration-300"
-                onClick={() => handleDownload(reportIndex)}
+              <section
+                className={`dark:bg-darkMode bg-primaryBlue ${
+                  reportIndex === 0 ? "mt-[5rem]" : "mt-5"
+                } mx-2 md:mx-[2rem] rounded-md overflow-hidden py-5 shadow-md`}
               >
-                Baixar como documento Word
-              </button>
+                <div className="flex flex-col items-start mx-5 gap-3 lg:flex-row lg:justify-between lg:items-top">
+                  <h1 className="max-w-[300px] md:max-w-[350px] font-bold text-xl">
+                    {report.schoolName}
+                  </h1>
+                  <p className="font-bold">{report.createdAt}</p>
+                </div>
+
+                <div className="flex flex-col items-start mx-2 mt-5 gap-3 p-3 lg:py-0">
+                  <p className="font-bold">
+                    Remetente:
+                    <span className="font-normal ml-3">
+                      {report.authorName}
+                    </span>
+                  </p>
+                  <p className="font-bold">
+                    Tema:
+                    <span className="font-normal ml-3">{report.theme}</span>
+                  </p>
+                  <p className="font-bold">
+                    Classe e turno:
+                    <span className="font-normal ml-3">
+                      {report.classAndShift}
+                    </span>
+                  </p>
+                  <p className="font-bold flex flex-wrap">
+                    Etapas realizadas nesta temática:
+                    {report.activitiesDone.map((activity, activityIndex) => (
+                      <span key={activityIndex} className="font-normal ml-3">
+                        {activityIndex === report.activitiesDone.length - 1
+                          ? `${activity}`
+                          : `${activity};`}
+                      </span>
+                    ))}
+                  </p>
+                  <p className="font-bold">
+                    N° de voluntários que participaram:
+                    <span className="font-normal ml-3">{report.coworkers}</span>
+                  </p>
+                  <p className="font-bold flex flex-wrap">
+                    Materiais utilizados:
+                    {report.resources.map((resource, resourceIndex) => (
+                      <span key={resourceIndex} className="font-normal ml-3">
+                        {resourceIndex === report.resources.length - 1
+                          ? `${resource}`
+                          : `${resource};`}
+                      </span>
+                    ))}
+                  </p>
+                  <p className="font-bold">
+                    N° de atendidos em capelania individual:
+                    <span className="font-normal ml-3">
+                      {report.assistedInChaplaincy}
+                    </span>
+                  </p>
+                  <p className="font-bold">
+                    Observação da capelania individual:
+                    <span className="font-normal ml-3">
+                      {report.chaplaincyObservation}
+                    </span>
+                  </p>
+                </div>
+              </section>
+              <DownloadReportTable report={report} />
             </div>
           ))}
 
