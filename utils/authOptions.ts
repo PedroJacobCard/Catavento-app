f37874f4a1c21cdfeda7bcdaf0f69fd70ce440a6
Pedro Henrique Jacob Cardoso <pedrojacob05cardoso@gmail.com>
2024-05-16 19:00:22 -0300

    working on types

--- utils/authOptions.ts
+++ utils/authOptions.ts
@@ -1,4 +1,4 @@
-import { AuthOptions } from "next-auth";
+import { AuthOptions, User } from "next-auth";
 import GoogleProvider from 'next-auth/providers/google';
 
 import prisma from "@/lib/prismadb";
@@ -20,39 +20,40 @@ export const authOptions: AuthOptions = {
     })
   ],
   callbacks: {
-    async signIn({user, account, credentials}) {
-     const userExist = await prisma.user.findUnique({
-       where: {
-         email: user.email
-       }
-     });
-
-     if (!userExist) {
-       toast.error("Ops! Parece que você ainda não está registrado.")
-       return redirect("/sign-up");
-     }
-
-     if (userExist) {
-      return userExist;
-     }
-     
-     const createUser = await prisma.user.create({
-       data: {
-         email: user.email,
-         name: user.name,
-         image: user.image,
-         account: account,
-         ...credentials
-       }
-     });
-
-     return createUser;
-    },
-    async session({ session, user }) {
-      if (session.user) {
-        const userAdapter: UserType | null = await prisma.user.findUnique({
+    async signIn({user, credentials}) {
+      if (user) {
+        const userExist = await prisma.user.findUnique({
           where: {
-            email: user.email
+            email: user.email  || ''
+          }
+        });
+      
+        if (!userExist) {
+          toast.error("Ops! Parece que você ainda não está registrado.")
+          return redirect("/sign-up");
+        }
+      
+        if (userExist) {
+         return userExist;
+        }
+        
+        const createUser: UserType | null = await prisma.user.create({
+          data: {
+            email: user.email,
+            name: user.name,
+            image: user.image,
+            ...credentials
+          }
+        });
+      
+        return createUser;
+      }
+    },
+    async session({ session }) {
+      if (session.user) {
+        const userAdapter = await prisma.user.findUnique({
+          where: {
+            email: session.user.email || ''
           }
         });
         if (userAdapter) {
