fbf442eb43ae4165bfec912bf95f0ca6392cc117
Pedro Henrique Jacob Cardoso <pedrojacob05cardoso@gmail.com>
2024-06-26 15:43:24 -0300

    updated style Privacy Policy

--- app/Politica-de-Privacidade/page.tsx
+++ app/Politica-de-Privacidade/page.tsx
@@ -1,20 +1,19 @@
 function PrivacyPolicy() {
   return (
     <div className="flex items-center justify-center w-full h-[100vh] dark:bg-darkModeBgColor bg-primaryBlue">
-      <div className="flex-col p-5 w-[50vw] h-[80vh] dark:bg-darkMode bg-secondaryBlue shadow-md rounded-md">
+      <div className="flex-col p-5 gap-3 w-[50vw] h-[80vh] dark:bg-darkMode bg-secondaryBlue shadow-md rounded-md">
         <p>
-          O Catavento é um projeto da MPC{" "}
-          <param name="MPC" value="Mocidade para Cristo" />   Goiânia, que visa o
+            O Catavento é um projeto da MPC Goiânia, que visa o
           apoio às escolas com o trabalho de valores fundamentais para o
           desenvolvimento social.
         </p>
         <p>
-          O App web nasce do desejo de tornar a organização do  projeto mais
+            O App web nasce do desejo de tornar a organização do  projeto mais
           acessível, permitindo que os voluntários tenham mais  conhecimento e
           envolvimento do trabalho realizado.
         </p>
         <p>
-          O App web não compartilha de dados pessoais com nenhuma organização fora do meio Catavento. Os dados fornecidos pela conta do Google são armazenados e utilizados pelo próprio usuário, para dar mais segurança e controle de acesso. Orientamos que ao utilizar este App, fique  ciente do compartilhamento de seu endereço de email e nome, fornecidos pelo Google, com os organizadores do  projeto Catavento.
+            O App web não compartilha de dados pessoais com nenhuma organização fora do meio Catavento. Os dados fornecidos pela conta do Google são armazenados e utilizados pelo próprio usuário, para dar mais segurança e controle de acesso. Orientamos que ao utilizar este App, fique  ciente do compartilhamento de seu endereço de email e nome, fornecidos pelo Google, com os organizadores do  projeto Catavento.
         </p>
       </div>
     </div>
